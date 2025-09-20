998433aa45f50fb820f1929fdffcc8736765c091
你的GitHub用户名 <online741520@gmail.com>
2025-09-20 21:23:12 +0800

    bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/DECO27-syoukai/',
   plugins: [
     vue(),
     vueDevTools(),
